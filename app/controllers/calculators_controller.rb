59091e90d4d2eb6fcde1db08d3887d038f100a84
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2025-03-30 16:12:31 +0000

    added busisness logic of result square root

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -3,4 +3,10 @@ class calcultorsController < applicationController
   def home
     render(template: "template/square")
   end
+
+  def square_result
+    @user_input = params.fetch("square_number").to_f
+    @square = @user_input ** 2
+    render(template: "templates/square_result")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/", {controller: "calcultors", action: "square"}
+  get "/square/new", { controller: "calcultors", action: "square" }
+
   
 end
