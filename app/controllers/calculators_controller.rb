cb6130a9a01a90359d9d42d22e5498f92e859d9b
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2025-03-30 16:27:33 +0000

    added busisness logic for payment

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,4 +1,4 @@
-class calcultorsController < applicationController
+class CalcultorsController < ApplicationController
   
   def home
     render(template: "template/square")
@@ -19,5 +19,24 @@ class calcultorsController < applicationController
     @square_root = Math.sqrt(@user_input_square.to_f)
     render(template: "templates/square_root_result")
   end
-  
+
+  def payment
+    render(template: "templates/payment")
+
+  end
+
+  def payment_result
+    @the_apr = params.fetch("apr").to_f/100/12
+    @the_years = params.fetch("years").to_i * 12
+    @the_principal = params.fetch("principal").to_f
+
+    numerator = @the_apr *  @the_principal
+    denominator = 1 - (1 + @the_apr) ** -@the_years
+    @the_payment = (numerator / denominator).round(2)
+    @interest_rate = (@the_apr *12 * 100).round(4)
+
+    render(template: "templates/payment_result")
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get "/square/new", { controller: "calcultors", action: "square" }
   get "/square_root/new", { controller: "calcultors", action: "square_root" }
   get "/square_root/results", { controller: "calcultors", action: "square_root_result" }
+  get "/payment/new", { controller: "calcultors", action: "payment" }
+  get "/payment/results", { controller: "calcultors", action: "payment_result" }
 
   
 end
